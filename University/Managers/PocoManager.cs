756a481db6c5ce63ec43d7140acba9477d228a94
oledb <zf0qwe@gmail.com>
2017-03-09 11:55:34 +0300

    AddPocoManager

---
+++ University/Managers/PocoManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Managers
+{
+    public abstract class PocoManager<T> where T : class
+    {
+        private IContextFactory factory;
+        internal PocoManager(IContextFactory contextFactory)
+        {
+            factory = contextFactory;
+        }
+
+        protected abstract void addObject(T obj, UniversityContext context);
+        protected abstract IQueryable<T> getObject(Func<T, bool> isValid,
+            UniversityContext context);
+        protected abstract void removeObject(T obj, UniversityContext context);
+
+        public void Add(T obj)
+        {
+            using (var context = factory.Create())
+            {
+                addObject(obj, context);
+                context.SaveChanges();
+            }
+        }
+
+        public List<T> Get()
+        {
+            using (var context = factory.Create())
+            {
+                return getObject(o => true, context).ToList();
+            }
+        }
+
+        public List<T> Get(Func<T, bool> isValid)
+        {
+            using (var context = factory.Create())
+                return getObject(isValid, context).ToList();
+        }
+
+        public T GetSingle(Func<T, bool> isValid)
+        {
+            using (var context = factory.Create())
+                return getObject(isValid, context).SingleOrDefault();
+        }
+    }
+}
