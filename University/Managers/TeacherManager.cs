997bb1c83ca0bf9166691d0ae904f4eaf1cb8b9e
oledb <zf0qwe@gmail.com>
2017-03-09 11:37:17 +0300

    Add TeacherManager

---
+++ University/Managers/TeacherManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University
+{
+    public class TeacherManager
+    {
+        public void Add(Teacher teacher)
+        {
+
+        }
+
+        public List<Teacher> Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ UniversityTest/TeacherManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using University;
+
+namespace UniversityTest
+{
+    [TestFixture]
+    class TeacherManagerTest
+    {
+        [SetUp]
+        public void CreateDB()
+        {
+            FakeContextFactory.RecreateDb();
+        }
+
+        [Test]
+        public void AddTeacherToDb()
+        {
+            //Arrange
+            var teacher = new Teacher()
+            {
+                Name = "Karl",
+                Surname = "Marx"
+            };
+            var teachers = new TeacherManager();
+
+            //Act
+            teachers.Add(teacher);
+            var output = teachers.Get();
+
+            //Assert
+            Assert.AreEqual(1, output.Count);
+            Assert.AreEqual("Marx", output[0].Surname);
+        }
+    }
+}
